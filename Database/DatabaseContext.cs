161d89513d9aa368f8d426657c55e4be8adeba34
mirkoSrsens <mirko.srsens@gmail.com>
2020-01-15 20:00:34 +0100

    as
    as

---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<ProductionResponse> ProductionResponse { get; set; }
+    public DbSet<ProductionSegmentResponse> ProductionSegmentResponse { get; set; }
+    public DatabaseContext()
+        : base("defaultCon")
+    {
+
+    }
+}
---
+++ Database/ProductionSegmentResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class ProductionSegmentResponse
+{
+    [Key]
+    public long Oid { get; set; }
+
+    public string ErrorMessage { get; set; }
+
+    public DateTime DateRecieved { get; set; }
+
+    public int Temperature { get; set; }
+
+    public ProductionResponse ProductionResponseModel { get; set; }
+}
